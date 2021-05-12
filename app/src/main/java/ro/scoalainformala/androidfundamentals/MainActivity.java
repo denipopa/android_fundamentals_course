b1d19b307d8d75bf737ae03d2c9ca3aabadf2da0
deni <denipopa199@gmail.com>
2021-05-12 17:19:14 +0300

    Challenge2

--- app/src/main/java/ro/scoalainformala/androidfundamentals/MainActivity.java
+++ app/src/main/java/ro/scoalainformala/androidfundamentals/MainActivity.java
@@ -3,6 +3,7 @@ package ro.scoalainformala.androidfundamentals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try{
+            int a=0;
+            int b=4/a;
+        }
+        catch (Exception e)
+        {
+            Log.e("HappyBirthday", "Exception: "+Log.getStackTraceString(e));
+        }
     }
 }
